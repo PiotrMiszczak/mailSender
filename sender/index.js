5fc75b101c82f4fc95362483c256350209144bda
Piotr Miszczak <miszpiotr@wp.pl>
2022-07-04 20:56:30 +0200

    updated env and package.json

--- sender/index.js
+++ sender/index.js
@@ -37,5 +37,5 @@ let mailer = nodemailer.createTransport({
       }
     )
   })
-  app.listen(5000, ()=>console.log('Server started at http://localhost:5000'))
+  app.listen(process.env.PORT, ()=>console.log(`Server started at http://localhost:${process.env.PORT}`))
 

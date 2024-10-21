e44b13bb7b0bc14c9e206df9a8650191971d5362
Piotr Miszczak <miszpiotr@wp.pl>
2024-10-21 20:45:57 +0200

    basic authorization

--- sender/index.js
+++ sender/index.js
@@ -33,21 +33,51 @@ let mailer = nodemailer.createTransport({
   },
 });
 
-app.post("/", function (req, res) {
+app.post("/", function (req,res) {
+  if(!isAuth(req))
+  {
+    return res.status(401).send("Request unauthorized");
+  }
   mailer.sendMail(
     {
+      
       from: process.env.MAIL_ADDRESS,
       to: process.env.MAIL_ADDRESS,
       subject: "New form submission",
       html: `<p>You've got new message from ${req.body.name}(${req.body.email}), who's looking to buy in ${req.body.timeFrame}:</p>
         <p>${req.body.other}</p>`,
-    },
+    }
+    ,
     function (err) {
       if (err) return res.status(500).send(err);
-      res.json({ success: true });
+      res.status(200).send("Success");
     }
   );
 });
 app.listen(process.env.PORT, () =>
   console.log(`Server started at http://localhost:${process.env.PORT}`)
 );
+
+const isAuth = (req) =>
+{
+  const auth = req.headers.authorization
+
+  if(auth && auth.indexOf('Basic ') !== -1)
+  {
+    const base64Credentials =  auth.split(' ')[1];
+    const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+    const [username, password] = credentials.split(':');
+  
+    if(password==process.env.SERVER_PASSWORD){
+      return true
+    }
+    else
+    {
+      return false
+    }
+
+  }
+  else{
+    return false
+  }
+}
